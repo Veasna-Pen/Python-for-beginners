1bbcad49ae8f49d9ef4546b9405ce44b377284cc
Veasna <Veasna3d@gmail.com>
2024-08-21 16:43:06 +0700

    init

---
+++ .venv/app.py
@@ -0,0 +1,143 @@
+# 1. Variable
+# age = 20
+
+# price = 12.12
+
+# first_name = 'veasna'
+
+# is_online = False
+
+# print(is_online)
+
+# 2. Get Value from input console
+
+# name = input("What is your first name? ")
+# print("Hello " + name)
+
+# 3. Type Conversion
+
+# birth_year = input("Enter your birth year: ")
+
+# int()
+# float()
+# bool()
+# str()
+
+# age = 2024 - int(birth_year)
+# print(age)
+
+# first = input("First: ")
+# first = float(input("First: "))
+
+# second = input("Second: ")
+# second = float(input("Second: "))
+
+# sum = float(first) + float(second)
+# sum = first + second
+
+# print("Result: " + str(sum))
+
+# 4. Strings
+
+# course = "Python for beginners"
+# print(course.upper())
+# print('Python' in course)
+
+# 5. Arithmetic Operators
+
+# print(10 + 3)
+
+# x = 10
+# x = x + 4
+# x += 4
+
+# 6. Operator Precedence
+# x = 10 + 3 * 2
+
+# x = (10 + 3) * 2
+# print(x)
+
+# 7. Comparison Operators
+
+# x = 3 > 2
+# print(x)
+
+# 8. Logical Operators
+
+# price = 25
+# print(price > 10 and price < 30)
+# print(price > 10 or price < 30)
+# print(not price > 10)
+
+# 9. if statements
+
+# temperature = 5
+# if temperature > 30:
+
+#     print("It's a hot day")
+
+# elif temperature > 20:
+
+#     print("It's a nice day")
+
+# else:
+
+#     print("It's cold")
+
+# print("Done")
+
+# weight = int(input("Weight: "))
+# unit = input("(K)g or (L)bs: ")
+
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+
+# 10. while loops
+# i = 1
+# while i <= 5:
+#     print(i * '*')
+#     i+=1
+
+# 11. Lists
+
+# names = ["Veasna", "Thida", "Mey"]
+# names[2] = "Mey Mey"
+# print(names[2])
+# print(names[0:2])
+
+# 12. List Methods
+
+# numbers = [1,2,3,4]
+# numbers.append(5)
+# numbers.insert(0, -1)
+# numbers.remove(1)
+# numbers.clear()
+
+# print(1 in numbers)
+# print(len(numbers))
+
+# 13. For loops
+
+# numbers = [1,2,3,4]
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+# 14. The range() Function
+
+# numbers = range(5, 10, 2)
+# for number in range(5):
+#     print(number)
+
+# 15. Tuples
+
+# numbers = (1,2,3,4)
+# numbers.count(3)
